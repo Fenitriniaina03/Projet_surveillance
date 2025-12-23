f026b6b9851a4e96300cd8f13e794da57e525c66
Fenitriniaina03 <ambinintsoafenitriniaina@gmail.com>
2025-12-23 13:17:53 +0300

    Agile Monitor: monorepo backend + frontend + gitignore

--- monitor-backend/monitor/serializers.py
+++
@@ -1,9 +0,0 @@
-# monitor/serializers.py
-from rest_framework import serializers
-from .models import SiteCheck
-
-class SiteCheckSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SiteCheck
-        fields = "__all__"
-
